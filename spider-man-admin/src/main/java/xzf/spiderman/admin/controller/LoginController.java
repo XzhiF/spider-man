7d532d5b40907dea5670338aa966424d1257a11d
xiezhifeng <124956282@qq.com>
2020-08-19 14:52:17 +0800

    remove session param

--- spider-man-admin/src/main/java/xzf/spiderman/admin/controller/LoginController.java
+++ spider-man-admin/src/main/java/xzf/spiderman/admin/controller/LoginController.java
@@ -2,6 +2,8 @@ package xzf.spiderman.admin.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 import xzf.spiderman.admin.data.LoginReq;
 import xzf.spiderman.admin.data.SessionAdminUser;
 import xzf.spiderman.admin.service.LoginService;
@@ -17,9 +19,11 @@ public class LoginController
     @Autowired
     private LoginService loginService;
 
+
     @PostMapping("/admin/login")
-    public Ret<SessionAdminUser> login(@Valid @RequestBody LoginReq req, HttpSession session)
+    public Ret<SessionAdminUser> login(@Valid @RequestBody LoginReq req)
     {
+        HttpSession session = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getRequest().getSession();
         SessionAdminUser data = loginService.login(req);
         session.setAttribute(SessionAdminUser.SESSION_KEY, data);
         return Ret.success(data);
--- spider-man-admin/src/main/java/xzf/spiderman/admin/controller/LogoutController.java
+++ spider-man-admin/src/main/java/xzf/spiderman/admin/controller/LogoutController.java
@@ -3,6 +3,8 @@ package xzf.spiderman.admin.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 import xzf.spiderman.admin.data.SessionAdminUser;
 import xzf.spiderman.common.Ret;
 
@@ -13,8 +15,9 @@ public class LogoutController
 {
 
     @RequestMapping(value = "/admin/logout", method = {RequestMethod.GET,RequestMethod.POST})
-    public Ret<Void> logout( HttpSession session)
+    public Ret<Void> logout()
     {
+        HttpSession session = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getRequest().getSession();
         session.removeAttribute(SessionAdminUser.SESSION_KEY);
         return Ret.success();
     }
